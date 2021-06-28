9c77a86d815965d530314934e74fd33c96894ad1
ellisa <ellisaissa@gmail.com>
2021-06-28 18:43:51 -0400

    notes
    edited to take out comments

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,10 @@
-// TODO: Include packages needed for this application
+
 
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
@@ -60,12 +59,11 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer.prompt(questions).then((inquirerResponses)=> {
         console.log("Creating ReadME");
@@ -73,5 +71,4 @@ function init() {
     })
 }
 
-// Function call to initialize app
 init();
